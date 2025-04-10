6285c2e66dce3ec200a00febf18215c180a28247
elderneverdie <48972159+elderneverdie@users.noreply.github.com>
2025-04-10 11:04:15 +0800

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 @pytest.fixture(scope="session")
 def driver():
     # 设置 Edge 驱动路径
-    edge_driver_path = r'E:\01Downloads\edgedriver_win64\msedgedriver.exe'
+    edge_driver_path = r'msedgedriver.exe'
     driver = webdriver.Edge(executable_path=edge_driver_path)
     driver.maximize_window()
     yield driver
